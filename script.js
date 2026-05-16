cff664e4ad7d258d2aa08755464d8adcdf09f2a7
guessthenumber72 <vividdaeco@gmail.com>
2026-05-16 08:18:00 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let SecretNumber = Math.floor(Math.random() * 100) + 1;
+console.log(SecretNumber);
+Attempts = 0;
+
+function guessNumber() {
+    let UserInput = document.getElementById("GuessInput").value;
+
+    let Guess = Number(UserInput);
+
+    Attempts++ + Guess;
+
+    console.log(Guess);
+
+    console.log(Attempts);
+
+    document.getElementById("Attempts").textContent = `Attempts: ${Attempts}`
+    if (Guess > SecretNumber) {
+        document.getElementById("Answer").textContent = "Too high!";
+
+    }
+    else if (Guess < SecretNumber) {
+        document.getElementById("Answer").textContent = "Too low!";
+    }
+    else if (Guess === SecretNumber) {
+        document.getElementById("Answer").textContent = 'Correct!';
+    }
+}
+
+function RestartGame() {
+    SecretNumber = Math.floor(Math.random() * 100) + 1;
+    Attempts = 0;
+    document.getElementById("GuessInput").value = " ";
+    document.getElementById("Attempts").textContent = `Attempts: ${Attempts}`
+}
